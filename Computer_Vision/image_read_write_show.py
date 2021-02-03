c8dd21c44856d6b16dea7f1809d7820cef85e92f
nikhil366 <upadhyaynikhil1308@gmail.com>
2021-02-03 14:41:22 +0530

    Computer vision use in image read and write

---
+++ Computer_Vision/image_read_write_show.py
@@ -0,0 +1,16 @@
+import cv2
+
+'''here this code is use for choose image from our systemand show in new window'''
+image_path = "/home/nikhil/Pictures/Webcam/nik.jpg"
+img = cv2.imread(image_path, -1)
+cv2.imshow('image', img)
+k = cv2.waitKey(0) & 0xFF
+
+'''by default k == 27 is value of Esc of our keyboard'''
+
+if k == 27:
+  cv2.destroyAllWindows()
+  '''ord is function  in which we can define and on press of this button video will save'''
+elif k == ord('s'):    
+  cv2.imwrite('image_copy', img)
+  cv2.destroyAllWindows()
